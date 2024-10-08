5b13193bafb545ee9824fc382db4a3f26d8371e8
AlecḰeppler <aleckeppler2016@gmail.com>
2024-10-08 18:55:23 -0300

    enviando alterações no projeto Sistema de Banco Parte 2

--- Sistema-de-Banco-Parte2.py
+++ Sistema-de-Banco-Parte2.py
@@ -1,5 +1,3 @@
-from xml.sax.saxutils import escape
-#Alteração Teste
 menu = """
     [1] Depositar
     [2] Sacar
@@ -7,12 +5,30 @@ menu = """
     [4] Sair
 
 --> """
-
+valor_saque = 0
 saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+def sacar(saldo, extrato,valor_saque, limite,numero_saques,LIMITE_SAQUES):
+    print("============== SACAR ==============")
+    if numero_saques < LIMITE_SAQUES:
+        numero_saques += 1
+        valor_saque = float(input("Digite o valor para saque: "))
+        if valor_saque > saldo:
+            print("Operação invalida, valor menor que o Saldo!")
+        elif valor_saque > limite:
+            print("O limite maximo para saque é de: R$ 500,00")
+        else:
+            saldo -= valor_saque
+            extrato += f"Saque: -R$ {valor_saque}\n"
+            print("Valor sacado com sucesso!")
+    else:
+        print("O limite de saques são 3x ao dia!")
+    return saldo, extrato
+
 
 while True:
     opcao = input(menu)
@@ -28,20 +44,9 @@ while True:
             print("Valor depositado com sucesso!")
 
     elif opcao == "2":
-        print("=== Sacar ===")
-        if numero_saques < LIMITE_SAQUES:
-            numero_saques += 1
-            valor_saque = float(input("Digite o valor para saque: "))
-            if valor_saque > saldo:
-                print("Operação invalida, valor menor que o Saldo!")
-            elif valor_saque > limite:
-                print("O limite maximo para saque é de: R$ 500,00")
-            else:
-                saldo -= valor_saque
-                extrato += f"Saque: -R$ {valor_saque}\n"
-                print("Valor sacado com sucesso!")
-        else:
-            print("O limite de saques são 3x ao dia!")
+        saldo, extrato = sacar(saldo, extrato,valor_saque, limite,numero_saques, LIMITE_SAQUES)
+        print(f"Saldo Atual: {saldo}")
+        print(f"\nExtrato: \n{extrato}")
 
     elif opcao == "3":
         print("=== Extrato ===")
